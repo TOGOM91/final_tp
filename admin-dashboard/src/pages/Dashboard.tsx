b587ded6858f1c29e833e944332c4d2338aec6ea
TOGOM91 <tom.farge@outlook.fr>
2025-11-05 10:11:50 +0100

    new front

--- admin-dashboard/src/pages/Dashboard.tsx
+++ admin-dashboard/src/pages/Dashboard.tsx
@@ -13,11 +13,7 @@ export default function Dashboard() {
 
   useEffect(() => {
 
-     console.log("=== DEBUG ENV ===");
-  console.log("API_URL:", API_URL);
-  console.log("import.meta.env.VITE_API_URL:", import.meta.env.VITE_API_URL);
-  console.log("import.meta.env:", import.meta.env);
-  console.log("=================");
+    
     const fetchDashboardData = async () => {
       try {
         const token = localStorage.getItem("token");
